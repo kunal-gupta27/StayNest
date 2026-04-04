e87daffab98ec9e45dc6fd6c1fb8d6a025f731d6
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-04-04 21:06:07 +0530

    user files

---
+++ Classroom/routes/user.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+
+//Index - users
+router.get("/",(req, res) => {
+    res.send("GET for users");
+});
+//Show - users
+router.get("/:id",(req, res)=>{
+    res.send("GET for show users");
+});
+//Post  - users
+router.post("/",(req, res)=>{
+    res.send("POST for users");
+});
+
+//DELETE - users
+router.delete("/:id",(req, res)=>{
+    res.send("DELETE for users");
+});
+
+module.exports = router;
