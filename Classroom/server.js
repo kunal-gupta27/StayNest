dc44aa0225e63004401e0d1562aa5f3259bf6215
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-04-05 09:20:52 +0530

    learning web cookies

--- Classroom/server.js
+++ Classroom/server.js
@@ -2,9 +2,36 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser  = require("cookie-parser");
 
+app.use(cookieParser("secretcode"));
+
+//learning about the signed cookie
+app.get("/getsignedcookie",(req, res)=>{
+    res.cookie("made-in", "India", {signed: true});
+    res.send("signed cookie sent");
+})
+
+app.get("/verify",(req, res)=>{
+    console.log(req.signedCookies); //for signed data k liye
+    res.send("verified!!");
+})
+
+// learning about the cookie parsar
+app.get("/getcookies", (req, res)=>{
+    res.cookie("madein", "india");
+    res.cookie("greet", "namaste");
+    res.send("sent you some cookies");
+});
+
+
+app.get("/greet",(req, res)=>{
+    let {name = "anonymous"} = req.cookies;
+    res.send(`Hi! ${name}`);
+})
 
 app.get("/",(req, res)=>{
+    console.log(req.cookies);  //for unsigned data k liye
     res.send("Hi, I am root");
 })
 
