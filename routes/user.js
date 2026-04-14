37842445e90a3d40f97b12668dc2fa00b0dfb7cd
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-04-14 23:42:09 +0530

    updating files

--- routes/user.js
+++ routes/user.js
@@ -3,19 +3,25 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
+const { savedRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res)=>{
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", wrapAsync(async(req, res)=>{
+router.post("/signup", wrapAsync(async(req, res, next)=>{
     try{
     let {username, email, password} = req.body;
     const newUser = new User({email, username});
     const registerUser = await User.register(newUser, password);
     console.log(registerUser);
-    req.flash("success", "Welcome to StayNest!!");
-    res.redirect("/listings");
+    req.login(registerUser, (err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success", "Welcome to StayNest!!");
+        res.redirect("/listings");
+    })
     }catch(e){
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -26,9 +32,20 @@ router.get("/login",(req, res)=>{
     res.render("users/login.ejs");
 });
 
-router.post("/login", passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), async(req, res)=>{
+router.post("/login",savedRedirectUrl, passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }), async(req, res)=>{
     req.flash("success", "welcome back to staynest!");
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 });
+
+router.get("/logout", (req, res, next)=>{
+    req.logout((err)=>{
+        if(err){
+            next(err);
+        }
+        req.flash("success","you are logged out!!");
+        res.redirect("/listings");
+    })
+})
 
 module.exports = router;
