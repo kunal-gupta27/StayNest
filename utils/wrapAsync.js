11772ee395d491d6a07ef75d4413a64e283323bc
𝐾𝑢𝑛𝑎𝑙 𝐺𝑢𝑝𝑡𝑎 <kunalgupta.cse2027@gmail.com>
2026-04-15 23:15:48 +0530

    Update wrapAsync.js

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,9 +1,8 @@
-function wrapAsync(fn){
-    return function(req, res, next){
-        fn(req, res, next).catch(next);
-    }
-}
-
+// function wrapAsync(fn){
+//     return function(req, res, next){
+//         fn(req, res, next).catch(next);
+//     }
+// }
 //OR
 
 module.exports = (fn) =>{
