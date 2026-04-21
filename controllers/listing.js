c50f3560983b7b2b59bf5f08aba0bf4fa1961fe4
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-04-21 20:13:22 +0530

    add url and filename

--- controllers/listing.js
+++ controllers/listing.js
@@ -29,6 +29,10 @@ module.exports.showListing  = async (req, res)=>{
 };
 
 module.exports.createListing = async (req, res, next) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    
+
         if (!req.body.listing) {
         // return res.send("Title is required!");
         throw new ExpressError(400, "Send valid data for listing")
@@ -37,6 +41,7 @@ module.exports.createListing = async (req, res, next) => {
     
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
+    newListing.image = {url, filename};
     await newListing.save();
     req.flash("success", "New Listing Created!!");
     res.redirect("/listings");
