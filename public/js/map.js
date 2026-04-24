49d669fa8844d5d59051817782c0325563acb58b
kunal-gupta27 <kunalgupta.cse2027@gmail.com>
2026-04-24 12:11:28 +0530

    adding popup in marker

--- public/js/map.js
+++ public/js/map.js
@@ -3,16 +3,15 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
         container: 'map', // container ID
         style: 'mapbox://styles/mapbox/streets-v12',
-        center: [77.2090, 28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
 });
 
-
-
-console.log(coordinates);
-
-
  // Create a default Marker and add it to the map.
-const marker = new mapboxgl.Marker()
-        .setLngLat(coordinates)  //Listing.gemoetry.coordinates
+const marker = new mapboxgl.Marker({ color: 'red'})
+        .setLngLat(listing.geometry.coordinates)  //Listing.gemoetry.coordinates
+        .setPopup(new mapboxgl.Popup({offset: 25})
+        .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be Provided after Booking</p>`))
         .addTo(map);
+
+       
